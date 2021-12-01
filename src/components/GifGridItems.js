9f3a0bf434c561c51886d444caf2cc260d813cb6
Marco Agudo <marcoagudo21@gmail.com>
2021-12-01 13:22:49 -0300

    First commit

---
+++ src/components/GifGridItems.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const GifGridItems = ({id, title, url}) => {
+    return (
+        <div className= 'card animate__animated animate__fadeIn'>
+            <img src={url} alt={title}/>
+            <p>{title} </p>
+        </div>
+    )
+}
